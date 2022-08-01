cb160b9131e30d4b3dca7d8fb75e00fa28806f4f
Snipie <8315338+Snipie@users.noreply.github.com>
2022-08-01 16:45:56 +0200

    Add a demo GIF

--- README.m
+++ README.m
@@ -1,5 +1,5 @@
 Work in progress
 
-![demo](https://github.com/Snipie/Todo-web-app/blob/main/demo.gif?raw=true)
+![demo](https://github.com/Snipie/Todo-web-app/blob/main/demo.gif)
 
 This a repository where I put my results of trying to develop a simple todo web application.
